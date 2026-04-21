9bb31d5286b0122d47aa98aaf6110e6e1e7a5337
Oki-F6 <ness268@vk.com>
2026-04-21 15:23:04 +0300

    Поменяла Image на img

--- src/components/FoodPage/Header.tsx
+++ src/components/FoodPage/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="hero-header">
       <h1>Что приготовим?</h1>
-      <Image src="images/apple.png" alt="Apple" width="42" height="42" />
+      <img src="images/apple.png" alt="Apple" width="42" height="42" />
     </header>
   );
 };
